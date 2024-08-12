9e3d04fdc3ee43570602cbabee9efd9a6d64b038
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-08-12 15:56:29 +0300

    finshed up exercise and checked all test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
-};
+const leapYears = function (year) {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  };
+  
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// const leapYears = function(startYear, endYear) {
+//     const leapYear = []
+
+
+// for(let year = startYear; year <= endYear; year++) {
+//     if ((year % 4 ===0 && year % 100 !==0) || (year % 400 === 0)) {
+//         leapYear.push(year);
+//     }
+// }
+//     return leapYear;
+
+// };
